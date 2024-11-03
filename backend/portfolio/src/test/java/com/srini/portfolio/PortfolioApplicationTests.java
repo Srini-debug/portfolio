de4f414bdd4145cb08ce647d2138ffbfceedf3d3
Srini-debug <srinivaasan_s@outlook.com>
2024-11-03 13:03:38 -0600

    Initial commit

---
+++ backend/portfolio/src/test/java/com/srini/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srini.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
